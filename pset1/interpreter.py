2b2561b8602bd3270684b9c506591ae3ead08b05
federicob-0 <bragionfederico@gmail.com>
2026-03-17 00:00:46 +0100

    Create interpreter.py

---
+++ pset1/interpreter.py
@@ -0,0 +1,19 @@
+def main():
+	expression = input("Expression: ")
+	x, y, z = expression.split(" ")
+	x, z = float(x), float(z)
+	result = calculate(x, y, z)
+	print(f"{result:.1f}")
+	
+	
+def calculate(n1, op, n2):
+	if op == "+":
+		return n1 + n2
+	elif op == "-":
+		return n1 - n2
+	elif op == "*":
+		return n1 * n2
+	else:
+		return n1 / n2
+		
+main()
