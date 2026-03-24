ef50786aa8d56f3dd562aff5f18d6fdb27cd672e
federicob-0 <bragionfederico@gmail.com>
2026-03-24 16:58:13 +0100

    Update coke.py

--- pset2/coke.py
+++ pset2/coke.py
@@ -1,17 +1,13 @@
-def main():
-	coke = 50
-	while coke > 0:
-		pay = int(input("Insert Coin: "))
-		if pay == 5 or pay == 10 or pay == 25:
-			coke -= pay
-			if coke > 0: 
-				print(f"Amount Due: {coke}")
-		else:
+coke = 50
+print(f"Amount Due: {coke}")
+
+while coke > 0:
+	pay = int(input("Insert Coin: "))
+	if pay == 5 or pay == 10 or pay == 25:
+		coke -= pay
+		if coke > 0: 
 			print(f"Amount Due: {coke}")
-	
-	if coke == 0:
-		print(f"Change Owed: {0}")
-	elif coke < 0:
-		print(f"Change Owed: {0 - coke}")
-		
-main()
+		else:
+			print(f"Change Owed: {0 - coke}")
+	else:
+		print(f"Amount Due: {coke}")
