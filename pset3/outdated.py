95c3c00399329ab936bd8dd6214bf0837173a162
federicob-0 <bragionfederico@gmail.com>
2026-04-10 18:52:56 +0200

    Create outdated.py

---
+++ pset3/outdated.py
@@ -0,0 +1,32 @@
+def main():
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	month, day, year = get_date("Date: ", months)
+	format_date(month, day, year, months)
+    
+def get_date(prompt, months):
+	while True:
+		date = input(prompt).title()
+		try:
+			month, day, year = date.split("/")
+			month, day, year = int(month), int(day), int(year)
+		except ValueError:
+			pass
+		else:
+			if year > 0 and 1 <= month <= 12 and 1 <= day <= 31:
+				return month, day, year
+		try:
+			month, day, year = date.split(" ")
+			day, year = int(day.replace(",", "")), int(year)
+		except ValueError:
+			pass
+		else:
+			if year > 0 and month in months and 1 <= day <= 31:
+				return month, day, year
+
+def format_date(month, day, year, months):
+	if month not in months:
+		print(f"{year:04}-{month:02}-{day:02}")
+	else:
+		print(f"{year:04}-{months.index(month)+1:02}-{day:02}")
+
+main()
